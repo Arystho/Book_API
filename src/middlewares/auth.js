1b25e5b8d54f20244e1a17147aedf53dd2f80ca6
Jordan Blin <jordanblin-83470@hotmail.com>
2024-09-17 11:59:54 +0200

    second commit

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,10 +1,10 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/user');
+const {User} = require('../models');
 require('dotenv').config();
 
 const authMiddleware = async (req, res, next) => {
     try {
-        const token = req.headers.authorizations.split(' ')[1];
+        const token = req.headers.authorization.split(' ')[1];
 
         if (!token) {
             return res.status(401).json({ message: 'No token provided' });
